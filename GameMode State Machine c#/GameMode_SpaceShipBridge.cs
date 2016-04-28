4b31acdd415f975f7e9e62e1d8aa701f5870a36c
mikepinto81 <mikepinto81@hotmail.com>
2016-04-28 09:05:16 -0400

    moved setting ID to the Manager class

--- GameMode State Machine c#/GameMode_SpaceShipBridge.cs
+++ GameMode State Machine c#/GameMode_SpaceShipBridge.cs
@@ -6,7 +6,7 @@ public class GameMode_SpaceShipBridge : GameMode_Base {
 
     public override void SetupAwake()
     {
-        ID = "SpaceShipBridge";
+       //
     }
 
     public override IEnumerator EnterMode()
